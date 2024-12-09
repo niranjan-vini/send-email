9cb804dba49e3f7e4c16e2425c5241a17cc0cffe
nirannajn <niranajngowda.583@gmail.com>
2024-12-09 16:07:57 +0530

    day=23, send emails using the form

--- sending_emali.py
+++ sending_emali.py
@@ -1,11 +1,11 @@
 import smtplib,ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "niranjangowda.583@gmail.com"
-    password = "ewbubfqjgccvybcl"
+    password =os.getenv("PASSWORD")
     receiver = "niranjangowda.583@gmail.com"
     context = ssl.create_default_context()
 
